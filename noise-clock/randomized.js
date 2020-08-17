556d9cd021ea1bce043103d5ec2182005fea6366
Maryam Ahmed <maryamahmed@berkeley.edu>
2020-08-16 17:40:22 -0700

    recording stimuli order

--- noise-clock/randomized.js
+++ noise-clock/randomized.js
@@ -1,9 +1,9 @@
 Qualtrics.SurveyEngine.addOnload(function()
 {
 	/*Place your JavaScript here to run when the page loads*/
-
 	//hides blue button
 	this.hideNextButton();
+
 
 
 	//random number generator function
@@ -14,9 +14,9 @@ Qualtrics.SurveyEngine.addOnload(function()
 	//automatic slideshow method
 	var trial = 1;
 	var shown = [];
+	var first = 1;
 	carousel();
-
-
+	
 	function carousel() {
 	  var i;
 	  var x = document.getElementsByClassName("mySlides");
@@ -27,23 +27,30 @@ Qualtrics.SurveyEngine.addOnload(function()
 		x[i].style.display = "none";
 	  }
 		
-	randint = getRandomInt(x.length);//gets randint [0, x.length - 1]	
+	randint = getRandomInt(x.length);//gets randint [0, x.length - 1]
 	if (trial % 2 == 0) {
-		console.log("hide" + trial);
+		//console.log("hide" + trial);
 		trial++;
 		x[randint].style.display = "none"
 		setTimeout(carousel, 500);
 		
 	} else {
-		console.log("show img" + trial);
+		//console.log("show img" + trial);
+
 		trial++;
 			if (shown.includes(randint) == false) {
 				x[randint].style.display = "block";
 				shown.push(randint);
-				console.log(randint);
+
 			}
 		setTimeout(carousel, 1000);
 	}	
-	
-	
+		console.log(shown);		
+		Qualtrics.SurveyEngine.setEmbeddedData( 'stim-order', shown.toString());
+		
+		 
 	}
+	
+	
+
+});
